5ef95fb7acca970b9270a57940247ad2f35c6efc
marcinsam <marcin.samulek@oktawave.com>
2021-07-30 17:11:22 +0200

    Fix k8s nodes creation process

--- resource_node.go
+++ resource_node.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/hashicorp/terraform/helper/schema"
-	swagger "github.com/oktawave-code/oks-sdk"
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/oktawave-code/odk"
+	swagger "github.com/oktawave-code/oks-sdk"
 )
 
 func resourceNode() *schema.Resource {
@@ -49,23 +49,48 @@ func resourceNodeCreate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*ClientConfig).oktaOKSClient()
 	auth := m.(*ClientConfig).getOKSAuth()
 
+	iaasClient := m.(*ClientConfig).oktaClient()
+	iaasAuth := m.(*ClientConfig).ctx
+
 	typeId := d.Get("type_id").(int)
 	subregionId := d.Get("subregion_id").(int)
 	clusterName := d.Get("cluster_name").(string)
 	nodes := prepareNodeList(float32(subregionId), float32(typeId))
-	details, resp, err := client.ClustersApi.ClustersInstancesNamePost(*auth, swagger.K44SNodesSpecification{nodes}, clusterName)
+	operations, resp, err := client.ClustersApi.ClustersInstancesNamePost(*auth, swagger.K44SNodesSpecification{nodes}, clusterName)
 
 	if err != nil {
 		if resp != nil && resp.StatusCode == http.StatusNotFound {
-			return fmt.Errorf("Resource Node. CREATE. Cluster by name %s was not found", clusterName)
+			return fmt.Errorf("Resource Node. CREATE. Cluster with name %s was not found", clusterName)
 		}
 		return fmt.Errorf("Resource Node. CREATE. Error creating node: %s", err)
 	}
 
-	tasDone, err := evaluateTask(client, *auth, clusterName, details[0])
-	if err != nil {
-		return fmt.Errorf("Resource Cluster Node. CREATE. %s", err)
+	if len(operations[0].Error_) > 0 {
+		return fmt.Errorf("Resource Node. CREATE. Error creating node: %s", operations[0].Error_)
 	}
+
+	ticketId := operations[0].Ticket.Id
+	ticket := odk.Ticket{
+		Id: int64(ticketId),
+	}
+
+	log.Printf("[INFO] Resource Node. CREATE. Post instance ticket was successfull created.")
+	log.Printf("[INFO] Resource Node. CREATE. Waiting for ticket progress = 100..")
+	//waiting for ticket status
+	ticket, err = evaluateTicket(iaasClient, iaasAuth, ticket)
+	if err != nil {
+		return fmt.Errorf("Resource Node. CREATE. Ticket retrieval error. %s", err)
+	}
+
+	if ticket.Status.Id == TICKET_STATUS__ERROR {
+		apiTicketStatusId := int(ticket.Status.Id)
+		return fmt.Errorf("Resource Node. CREATE. Unable to create instance. Ticket status: %s", strconv.Itoa(apiTicketStatusId))
+	}
+
+	// tasDone, err := evaluateTask(client, *auth, clusterName, operation[0])
+	// if err != nil {
+	// 	return fmt.Errorf("Resource Cluster Node. CREATE. %s", err)
+	// }
 	nodeList, resp, err := client.ClustersApi.ClustersInstancesNameGet(*auth, clusterName)
 
 	if err != nil {
@@ -75,7 +100,7 @@ func resourceNodeCreate(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("Resource Node. READ. Error occured while retrieving clusters nodes: %s", err)
 	}
 
-	existedNode, err := retrieveNodeByName(nodeList, tasDone.InstanceName)
+	existedNode, err := retrieveNodeByName(nodeList, ticket.ObjectName)
 	if err != nil {
 		return fmt.Errorf("Resource Node. READ. Erroor occured while retrieving node from clusters node list: %s", err)
 	}
@@ -125,10 +150,12 @@ func resourceNodeDelete(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Resource Kubernetes Node. Delete. Initializing")
 	client := m.(*ClientConfig).oktaOKSClient()
 	auth := m.(*ClientConfig).getOKSAuth()
+	iaasClient := m.(*ClientConfig).oktaClient()
+	iaasAuth := m.(*ClientConfig).ctx
 	clusterName := d.Get("cluster_name").(string)
 	id, err := strconv.Atoi(d.Id())
-	nodeList := swagger.K44SNodesList{[]float32{float32(id)}}
-	task, resp, err := client.ClustersApi.ClustersInstancesNameDelete(*auth, nodeList, clusterName)
+	nodeList := swagger.K44SNodesList{[]float64{float64(id)}}
+	operations, resp, err := client.ClustersApi.ClustersInstancesNameDelete(*auth, nodeList, clusterName)
 
 	if err != nil {
 		if resp != nil && resp.StatusCode == http.StatusNotFound {
@@ -136,10 +163,34 @@ func resourceNodeDelete(d *schema.ResourceData, m interface{}) error {
 		}
 		return fmt.Errorf("Resource Node. DELETE. Error deleting Nodes: %s", err)
 	}
-	_, err = evaluateTask(client, *auth, clusterName, task[0])
-	if err != nil {
-		return fmt.Errorf("Resource Node. DELETE. Error while waiting for node destroying: %s", err)
+
+	if len(operations[0].Error_) > 0 {
+		return fmt.Errorf("Resource Node. CREATE. Error creating node: %s", operations[0].Error_)
 	}
+
+	// _, err = evaluateTask(client, *auth, clusterName, task[0])
+	// if err != nil {
+	// 	return fmt.Errorf("Resource Node. DELETE. Error while waiting for node destroying: %s", err)
+	// }
+
+	ticketId := operations[0].Ticket.Id
+	ticket := odk.Ticket{
+		Id: int64(ticketId),
+	}
+
+	log.Printf("[INFO] Resource Node. CREATE. Post instance ticket was successfull created.")
+	log.Printf("[INFO] Resource Node. CREATE. Waiting for ticket progress = 100..")
+	//waiting for ticket status
+	ticket, err = evaluateTicket(iaasClient, iaasAuth, ticket)
+	if err != nil {
+		return fmt.Errorf("Resource Node. CREATE. Ticket retrieval error. %s", err)
+	}
+
+	if ticket.Status.Id == TICKET_STATUS__ERROR {
+		apiTicketStatusId := int(ticket.Status.Id)
+		return fmt.Errorf("Resource Node. CREATE. Unable to create instance. Ticket status: %s", strconv.Itoa(apiTicketStatusId))
+	}
+
 	d.SetId("")
 	return nil
 }
@@ -147,28 +198,28 @@ func resourceNodeDelete(d *schema.ResourceData, m interface{}) error {
 //prepares node list with given subregion and type
 func prepareNodeList(subregionId float32, typeId float32) []swagger.Node {
 	nodeList := make([]swagger.Node, 1)
-	nodeList[0] = swagger.Node{subregionId, typeId}
+	nodeList[0] = swagger.Node{float64(subregionId), float64(typeId)}
 
 	return nodeList
 }
 
-func evaluateTask(client swagger.APIClient, auth context.Context, clusterName string, task swagger.K44sTaskDto) (swagger.K44sTaskDto, error) {
+// func evaluateTask(client swagger.APIClient, auth context.Context, clusterName string, task swagger.K44sTaskDto) (swagger.K44sTaskDto, error) {
 
-	for {
-		foundTask, resp, err := client.ClustersApi.ClustersInstancesNameTasksTaskIdGet(auth, task.TaskId, clusterName)
-		if err != nil {
-			if resp != nil && resp.StatusCode == 404 {
-				return swagger.K44sTaskDto{}, fmt.Errorf("Cluster by name %s was not found", clusterName)
-			}
-			return swagger.K44sTaskDto{}, fmt.Errorf("Error occured while retrieving cluster tasks: %s", err)
-		}
-		if strings.ToLower(foundTask.Status) == "failed" {
-			return swagger.K44sTaskDto{}, fmt.Errorf("Task status: FAILED")
-		}
-		log.Printf("[DEBUG] foundTask status, id, subregion and type %s, %s, %s, %s", strings.ToLower(foundTask.Status), strconv.Itoa(int(foundTask.InstanceId)), strconv.Itoa(int(foundTask.SubregionId)), strconv.Itoa(int(foundTask.TypeId)))
-		if foundTask.Status == "Succeeded" {
-			return foundTask, nil
-		}
-		time.Sleep(10 * time.Second)
-	}
-}
+// 	for {
+// 		foundTask, resp, err := client.ClustersApi.ClustersInstancesNameTasksTaskIdGet(auth, task.TaskId, clusterName)
+// 		if err != nil {
+// 			if resp != nil && resp.StatusCode == 404 {
+// 				return swagger.K44sTaskDto{}, fmt.Errorf("Cluster by name %s was not found", clusterName)
+// 			}
+// 			return swagger.K44sTaskDto{}, fmt.Errorf("Error occured while retrieving cluster tasks: %s", err)
+// 		}
+// 		if strings.ToLower(foundTask.Status) == "failed" {
+// 			return swagger.K44sTaskDto{}, fmt.Errorf("Task status: FAILED")
+// 		}
+// 		log.Printf("[DEBUG] foundTask status, id, subregion and type %s, %s, %s, %s", strings.ToLower(foundTask.Status), strconv.Itoa(int(foundTask.InstanceId)), strconv.Itoa(int(foundTask.SubregionId)), strconv.Itoa(int(foundTask.TypeId)))
+// 		if foundTask.Status == "Succeeded" {
+// 			return foundTask, nil
+// 		}
+// 		time.Sleep(10 * time.Second)
+// 	}
+// }
